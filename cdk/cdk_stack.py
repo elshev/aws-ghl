2a6c9605c311a79efc0b7b23ef5c78b45fa4bfdd
elshev <elshev@gmail.com>
2023-05-01 14:48:41 +0300

    cdk_stack.py: Add resources

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,31 +1,45 @@
+from constructs import Construct
 from aws_cdk import (
+    Stack,
     aws_iam as iam,
-    aws_lambda as lambda_,
+    aws_lambda as _lambda,
+    aws_events as events,
     aws_apigateway as apigw,
     aws_applicationinsights as appinsights,
     aws_resourcegroups as rg,
-    aws_ssm as ssm,
-    core
+    Duration,
+    Fn
 )
 
 
-class GoHighLevelStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, stage: str, **kwargs) -> None:
+class GoHighLevelStack(Stack):
+
+    @property
+    def python_runtime(self):
+        return self._python_runtime
+
+    def __init__(self, scope: Construct, id: str, stage: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        self._python_runtime = _lambda.Runtime.PYTHON_3_9
+
         # Create IAM role for Lambda functions
-        lambda_role = iam.Role(
+        ghl_lambda_role = iam.Role(
             self, "GhlLambdaRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
             managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambdaRole"),
-                iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambdaExecute"),
-                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMFullAccess")
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AWSLambdaRole"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AWSLambdaExecute"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "AmazonSSMFullAccess")
             ]
         )
 
         # Create IAM policy for Lambda logs
         lambda_logs_policy = iam.PolicyStatement(
+            sid='LambdaLogs',
             effect=iam.Effect.ALLOW,
             actions=[
                 "logs:CreateLogGroup",
@@ -36,8 +50,9 @@ class GoHighLevelStack(core.Stack):
             resources=["arn:aws:logs:*:*:*"]
         )
 
-        # Create IAM policy for S3 access
-        lambda_s3_policy = iam.PolicyStatement(
+        # Create IAM policy for S3 Object access
+        lambda_s3_object_policy = iam.PolicyStatement(
+            sid='S3Object',
             effect=iam.Effect.ALLOW,
             actions=[
                 "s3:DeleteObjectTagging",
@@ -58,36 +73,84 @@ class GoHighLevelStack(core.Stack):
             ],
             resources=["arn:aws:s3:::*/*"]
         )
-        lambda_s3_policy.add_resources("arn:aws:s3:::*")
+
+        # Create IAM policy for S3 Bucket access
+        lambda_s3_bucket_policy = iam.PolicyStatement(
+            sid='S3Bucket',
+            effect=iam.Effect.ALLOW,
+            actions=[
+                "s3:PutBucketTagging",
+                "s3:GetBucketLogging",
+                "s3:ListBucket",
+                "s3:GetBucketLocation",
+                "s3:GetBucketPolicy"
+            ],
+            resources=["arn:aws:s3:::*"]
+        )
 
         # Add policies to the IAM role
-        lambda_role.add_to_policy(lambda_logs_policy)
-        lambda_role.add_to_policy(lambda_s3_policy)
+        ghl_lambda_role.add_to_policy(lambda_logs_policy)
+        ghl_lambda_role.add_to_policy(lambda_s3_object_policy)
+        ghl_lambda_role.add_to_policy(lambda_s3_bucket_policy)
+
+        # Add a schedule event to trigger the token refresh function
+        refresh_token_schedule_rule = events.Rule(
+            self, 'GhlRefreshTokenSchedule',
+            schedule=events.Schedule.rate(Duration.hours(23)),
+            enabled=True,
+            description='A schedule for getting new Access and Rerfresh Tokens from GoHighLevel'
+        )
+
+        # Create the Lambda function for refreshing Access and Refresh tokens
+        _lambda.Function(
+            self, "GhlRefreshTokenFunction",
+            code=_lambda.Code.from_asset("src"),
+            handler="ghl_refresh_token.lambda_handler",
+            runtime=self.python_runtime,
+            role=ghl_lambda_role,
+            timeout=Duration.seconds(180),
+            description="Refreshes token as described here https://highlevel.stoplight.io/docs/integrations/00d0c0ecaa369-get-access-token. Stores new Access and Refresh Token in SSM Parameter Store",
+            architecture=_lambda.Architecture.X86_64,
+            events={refresh_token_schedule_rule},
+        )
+        # refresh_token_function.add_event_source(event_rule)
+
+
+        # Create the Lambda function as a WebHook for Conversation Unread event
+        ghl_hook_function = _lambda.Function(
+            self, "GhlHookFunction",
+            code=_lambda.Code.from_asset("src"),
+            handler="ghl_hook.lambda_handler",
+            runtime=self.python_runtime,
+            role=ghl_lambda_role,
+            timeout=Duration.seconds(180),
+            description="WebHook for GoHighLevel ConversationUnread event",
+            architecture=_lambda.Architecture.X86_64
+        )
 
         # Create the REST API using API Gateway
-        rest_api = apigw.RestApi(
+        ghl_api = apigw.LambdaRestApi(
             self, "GoHighLevelApi",
             rest_api_name="gohighlevel",
             deploy_options={
                 "stage_name": stage
-            }
+            },
+            endpoint_configuration=apigw.EndpointConfiguration(types=[apigw.EndpointType.REGIONAL]),
+            handler=ghl_hook_function
         )
 
-        # Create the Lambda function for refreshing tokens
-        refresh_token_function = lambda_.Function(
-            self, "GhlRefreshTokenFunction",
-            code=lambda_.Code.from_asset("src"),
-            handler="ghl_refresh_token.lambda_handler",
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            role=lambda_role,
-            timeout=core.Duration.seconds(120),
-            memory_size=128,
-            description="Refreshes token as described here https://highlevel.stoplight.io/docs/integrations/00d0c0ecaa369-get-access-token. Stores new Access and Refresh Token in SSM Parameter Store",
-            
+        resource_ghl = ghl_api.root.add_resource('gohighlevel')
+        resource_ghl.add_method('POST')
+
+        # Resource group
+        app_resource_group = rg.CfnGroup(
+            self, 'ApplicationResourceGroup',
+            name=Fn.sub(f'ApplicationInsights-SAM-${Stack.stack_name}'),
         )
 
-        # Add a schedule event to trigger the token refresh function
-        # refresh_token_function.add_event_source(
-        #     lambda_.events.Rule(
-        #         self, "GhlRefreshTokenSchedule",
-        #         schedule=lambda_.events.Schedule.rate(core.Duration.hours(23))
+        # Application Insights monitoring
+        appinsights.CfnApplication(
+            self, 'ApplicationInsightsMonitoring',
+            resource_group_name=app_resource_group.name,
+            auto_configuration_enabled=True
+        )
